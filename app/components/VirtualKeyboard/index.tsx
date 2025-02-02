f8e0230d1a97e26325300d79bab4138fbfa7c33b
Alexandre Batistella Bellas <alexandre.bellas@gmail.com>
2025-02-02 20:55:27 -0300

    fix: disable option button when there are at least 9 of it

--- app/components/VirtualKeyboard/index.tsx
+++ app/components/VirtualKeyboard/index.tsx
@@ -1,13 +1,26 @@
-import { useGameDispatch } from "@/contexts/GameProvider"
+import { useGame, useGameDispatch } from "@/contexts/GameProvider"
 import { useCallback } from "react"
 import { Button, ButtonText } from "../ui/button"
 
 export default function VirtualKeyboard() {
     // #region Contexts
+    const { board } = useGame()
     const gameDispatch = useGameDispatch()
     // #endregion
 
     // #region Callbacks
+    const isOptionDisabled = useCallback(
+        (option: number) => {
+            return (
+                board.reduce(
+                    (acc, row) => acc + row.reduce((rowAcc, tile) => rowAcc + (tile.value === option ? 1 : 0), 0),
+                    0
+                ) >= 9
+            )
+        },
+        [board]
+    )
+
     const handlePlaceNumberForSelectedTile = useCallback(
         (value: number) => {
             gameDispatch({ type: "set-value-for-selected-tile", value })
@@ -22,7 +35,9 @@ export default function VirtualKeyboard() {
                 <Button
                     key={number}
                     onPress={() => handlePlaceNumberForSelectedTile(number)}
-                    className="bg-blue-100 text-blue-500 px-2 sm:aspect-square"
+                    className="bg-blue-100 text-blue-500 px-2 sm:aspect-square 
+                        disabled:bg-gray-200 disabled:text-gray-500"
+                    disabled={isOptionDisabled(number)}
                 >
                     <ButtonText className="text-lg font-normal sm:text-xl">{number}</ButtonText>
                 </Button>
