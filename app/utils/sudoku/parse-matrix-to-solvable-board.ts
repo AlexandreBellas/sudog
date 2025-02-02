6967f9bf509f2594de10dd427bc3a348ce5a8a2d
Alexandre Batistella Bellas <alexandre.bellas@gmail.com>
2025-02-02 19:07:46 -0300

    fix: make default tiles with `isClue = false` when `value = null` in `parseMatrixToSolvableBoard`

--- app/utils/sudoku/parse-matrix-to-solvable-board.ts
+++ app/utils/sudoku/parse-matrix-to-solvable-board.ts
@@ -2,7 +2,7 @@ import { ISaveableBoard } from "@/services/IBoardGateway"
 import { solveBoard } from "./solve-board"
 
 export function parseMatrixToSolvableBoard(matrix: (number | null)[][]): ISaveableBoard {
-    const board = matrix.map((row) => row.map((value) => ({ value, isClue: true, notes: [] })))
+    const board = matrix.map((row) => row.map((value) => ({ value, isClue: !!value, notes: [] })))
     const solvedBoard = solveBoard(board)
 
     if (!solvedBoard.hasBeenSolved) {
