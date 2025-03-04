dca05399d951796ac990d73dd05e205accddee52
Alexandre Batistella Bellas <alexandre.bellas@gmail.com>
2025-03-04 16:03:40 -0300

    feat: add middleware for rate limiting for API

--- server/routes/web.php
+++ server/routes/web.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\BoardsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('boards/random', [BoardsController::class, 'random']);
-Route::post('boards/generate', [BoardsController::class, 'generate']);
+Route::middleware(['throttle:api'])->group(function () {
+    Route::get('boards/random', [BoardsController::class, 'random']);
+    Route::post('boards/generate', [BoardsController::class, 'generate']);
+});
