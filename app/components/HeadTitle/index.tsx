1b142f806f747786be98639884b1ebef9915b145
Alexandre Batistella Bellas <alexandre.bellas@gmail.com>
2025-03-08 09:30:56 -0300

    feat: add white background to HeadTitle

--- app/components/HeadTitle/index.tsx
+++ app/components/HeadTitle/index.tsx
@@ -3,7 +3,7 @@ import { Text } from "../ui/text"
 
 export default function HeadTitle() {
     return (
-        <Box className="flex flex-col items-center rounded-sm p-1 w-full">
+        <Box className="flex flex-col items-center rounded-md py-1 px-4 bg-white">
             <Text className="text-2xl font-bold text-gray-700">🐕 Sudog</Text>
             <Text className="text-sm italic">A sudoku game dog-themed</Text>
         </Box>
